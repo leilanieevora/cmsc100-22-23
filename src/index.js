607c7fba4a75943371129dca000c841bf81cb6e9
lfevora <lfevora@up.edu.ph>
2022-10-07 03:22:35 +0000

    refactor: move fastify to app.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import Fastify, { fastify } from 'fastify';
+import { build } from './app.js';
 
-async function start(){
-  try{
-    const fastify = Fastify({ logger: true });
-
-    fastify.get('/api', async (request, reply) => {
-      return {success : true}
-    });
+async function start () {
+  try {
+    const fastify = await build();
 
     const addr = await fastify.listen({
       port: '8080'
-    })
+    });
     console.log('Listening on ${addr}');
-  }catch (error){
+  } catch (error) {
     console.error(error);
     process.exit(1);
   }
